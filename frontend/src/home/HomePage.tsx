528311004ce8789a7b103ee16ca2fd64bb781933
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-08-21 00:23:31 +0530

    fix

--- frontend/src/home/HomePage.tsx
+++ frontend/src/home/HomePage.tsx
@@ -2,10 +2,12 @@ import CollectionsSection from './components/CollectionsSection'
 import FaqSection from './components/FaqSection'
 import HeroSlider from './components/HeroSlider'
 import Newsletter from './components/Newsletter'
+import NewArrivalsSection from './components/NewArrivalsSection'
 import OccasionSection from './components/OccasionSection'
 import StoreFooter from './components/StoreFooter'
 import StoreHeader from './components/StoreHeader'
 import StorySection from './components/StorySection'
+import './home.css'
 
 export default function HomePage() {
   return <div className="storefront"><StoreHeader />
@@ -13,6 +15,7 @@ export default function HomePage() {
       <HeroSlider />
       <StorySection />
       <CollectionsSection />
+      <NewArrivalsSection />
       <OccasionSection />
       <FaqSection />
       <Newsletter />
