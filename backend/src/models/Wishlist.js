342a28a50940ff60ee38298a0867dd33ccd63fbf
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-08-21 00:23:01 +0530

    fix

--- backend/src/models/Wishlist.js
+++ backend/src/models/Wishlist.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const wishlistItemSchema = new mongoose.Schema(
   {
     product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    variantId: { type: mongoose.Schema.Types.ObjectId },
+    quantity: { type: Number, required: true, min: 1, default: 1 },
   },
   { timestamps: true }
 );
