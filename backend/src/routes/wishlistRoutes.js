3297601fe2fdd1fa714d07b69fd5e743144b1969
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-08-21 00:23:06 +0530

    fix

--- backend/src/routes/wishlistRoutes.js
+++ backend/src/routes/wishlistRoutes.js
@@ -5,6 +5,7 @@ const { protect } = require('../middleware/auth');
 router.use(protect);
 router.get('/', controller.getWishlist);
 router.post('/items', controller.addItem);
+router.patch('/items/:id', controller.updateItem);
 router.delete('/items/:id', controller.deleteItem);
 
 module.exports = router;
