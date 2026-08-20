59e88bfcc9808c865290e8f6b40f2b1f460e21e7
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-08-21 00:23:23 +0530

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import type { User } from './admin/types'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './home/HomePage'
 import { CartPage, CheckoutPage, CollectionPage, ProductPage, SuccessPage, WishlistPage } from './store/StorePages'
+import CustomerLoginPage from './store/CustomerLoginPage'
 import AdminLayout from './admin/components/AdminLayout'
 import Login from './admin/components/Login'
 import OverviewPage from './admin/pages/OverviewPage'
@@ -20,9 +21,11 @@ export default function App() {
   return <BrowserRouter><Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/collections" element={<CollectionPage />} />
+    <Route path="/collections/all" element={<CollectionPage />} />
     <Route path="/products/:slug" element={<ProductPage />} />
     <Route path="/cart" element={<CartPage />} />
     <Route path="/wishlist" element={<WishlistPage />} />
+    <Route path="/login" element={<CustomerLoginPage />} />
     <Route path="/checkout" element={<CheckoutPage />} />
     <Route path="/order-success" element={<SuccessPage />} />
     <Route path="/admin" element={token && user ? <AdminLayout user={user} onSignOut={signOut} /> : <Login onSuccess={signIn} />}>
