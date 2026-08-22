668ce91a93cd2185b9c7d94bc5ded76297119466
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-08-22 20:12:50 +0530

    fix

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,12 @@
+export { default as CollectionPage } from './CollectionPage'
+export { default as ProductPage } from './ProductPage'
+export { default as CartPage } from './CartPage'
+export { default as WishlistPage } from './WishlistPage'
+export { default as CheckoutPage } from './CheckoutPage'
+export { default as SuccessPage } from './SuccessPage'
+export { default as CustomerLoginPage } from './CustomerLoginPage'
+export { default as StoreShell, Shell } from './StoreShell'
+
+export * from './types'
+export * from './utils'
+
